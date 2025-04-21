347fb008b191ef16f14e18a3b4f7a30f491b94e0
javiorfo <javierorfo@prontonmail.com>
2025-04-21 17:05:20 -0300

    changes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 *Personal [suckless DWM](https://dwm.suckless.org/) configuration*
 
 ## Dependencies
-- Firefox, st, Zathura, lf, Iosevka Nerd Font
+- Librewolf, alacritty, Zathura, thunar, Iosevka Nerd Font
 
 ## Patches
 - [Always Center](https://dwm.suckless.org/patches/alwayscenter/dwm-alwayscenter-20200625-f04cac6.diff)
--- config.h
+++ config.h
@@ -70,9 +70,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *browser[]  = { "firefox", NULL };
-static const char *filemanager[]  = { "st", "-e", "lf", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browser[]  = { "librewolf", NULL };
+static const char *filemanager[]  = { "thunar", NULL };
 static const char *fileviewer[]  = { "zathura", NULL };
 static const char *poweroff[]  = { "sudo", "systemctl", "poweroff",  NULL };
 static const char *reboot[]  = { "sudo", "systemctl", "reboot",  NULL };
